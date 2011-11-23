7ca5f1cd99f0653ca43b0dce79dca2465fc4c058
Oskar Thornblad <oskar.thornblad@gmail.com>
2011-11-23 23:17:20 +0100

    Rearranged some texts

--- XmlStreamer.php
+++ XmlStreamer.php
@@ -1,22 +1,7 @@
 <?php
-
-/*
-Author: oskar.thornblad@gmail.com
- 
-Usage:
-Extend the class and implement the processNode method.
-
-Example:
-class SimpleXmlStreamer extends XmlStreamer {	public function processNode($xmlString, $elementName, $nodeIndex) {
-		$xml = simplexml_load_string($xmlString);		$something = (string)$xml->Something->SomethingElse->ReadThis;		echo "$nodeIndex: Extracted string '$something' from parent node '$elementName'\n";		
-		return true;	}
-}
-$streamer = new SimpleXmlStreamer("myLargeXmlFile.xml");
-if ($streamer->parse()) {	echo "Finished successfully";
-} else {
-	echo "Couldn't find root node";
-}
-*/
+// XmlStreamer 1.0
+// oskar.thornblad@gmail.com
+// Free for everyone for everything, attribution voluntary
 
 abstract class XmlStreamer {
 	private $handle;
